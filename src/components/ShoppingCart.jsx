c5efa6363e171fda49114a0c46b04402135edb9e
degemarps <degemarpereira@gmail.com>
2022-01-20 19:08:42 -0300

    Requisito 2 em andamento

---
+++ src/components/ShoppingCart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ShoppingCart extends React.Component {
+  render() {
+    return (
+      <section>
+        <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3>
+      </section>
+    );
+  }
+}
+
+export default ShoppingCart;
