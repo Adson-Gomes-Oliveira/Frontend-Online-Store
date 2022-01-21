80d4c5d32b7483461298e00014df053012375db8
degemarps <degemarpereira@gmail.com>
2022-01-21 14:54:48 -0300

    Requisito 4 feito

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,13 +1,40 @@
 // Esse código foi desenvolvido em conjunto( Luiz e Adson)
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { getCategories } from '../services/api';
 import Home from './Home';
 import ShoppingCart from './ShoppingCart';
 
 export default class Content extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+
+    this.getCategoriesList = this.getCategoriesList.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCategoriesList();
+  }
+
+  async getCategoriesList() {
+    const categoriesReceived = await getCategories();
+    this.setState({ categories: [...categoriesReceived] });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
+        { categories.map((categorie) => (
+          <label data-testid="category" key={ categorie.name } htmlFor={ categorie.id }>
+            <input id={ categorie.id } type="radio" />
+            { categorie.name }
+          </label>
+        )) }
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route path="/cart" component={ ShoppingCart } />
