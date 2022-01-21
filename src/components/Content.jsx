d72e385311f7e9b27d9bf37951b2a359d2989d98
degemarps <degemarpereira@gmail.com>
2022-01-21 15:22:16 -0300

    Adicaçao do css

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import { getCategories } from '../services/api';
 import Home from './Home';
 import ShoppingCart from './ShoppingCart';
+import '../css/Content.css';
 
 export default class Content extends Component {
   constructor() {
@@ -28,17 +29,21 @@ export default class Content extends Component {
   render() {
     const { categories } = this.state;
     return (
-      <div>
-        { categories.map((categorie) => (
-          <label data-testid="category" key={ categorie.name } htmlFor={ categorie.id }>
-            <input id={ categorie.id } type="radio" />
-            { categorie.name }
-          </label>
-        )) }
-        <Switch>
-          <Route exact path="/" component={ Home } />
-          <Route path="/cart" component={ ShoppingCart } />
-        </Switch>
+      <div className="main-page">
+        <section className="categories">
+          { categories.map((categorie) => (
+            <label data-testid="category" key={ categorie.name } htmlFor={ categorie.id }>
+              <input id={ categorie.id } type="radio" />
+              { categorie.name }
+            </label>
+          )) }
+        </section>
+        <section className="products">
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route path="/cart" component={ ShoppingCart } />
+          </Switch>
+        </section>
       </div>
     );
   }
