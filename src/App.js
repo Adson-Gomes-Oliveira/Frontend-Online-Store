af15607699cf36fab84bf8d15f54a16fec3772e6
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2022-01-23 17:17:08 -0300

    alteraçoes requisito 02

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import ShoppingCart from './components/ShoppingCart';
 
 class App extends React.Component {
   render() {
