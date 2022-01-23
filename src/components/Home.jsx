ac40db9fa6510ea2b6aced3f395b7e81307a75b1
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2022-01-23 18:04:40 -0300

    alteraçoes requisito 04

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,30 @@
 // Esse código foi desenvolvido em conjunto( Luiz e Adson)
 import React, { Component } from 'react';
 import Header from './Header';
+import * as api from '../services/api';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+
+    this.getCategoriesList = this.getCategoriesList.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCategoriesList();
+  }
+
+  getCategoriesList = async () => {
+    const categorieList = await api.getCategories();
+    this.setState({ categories: categorieList });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <section>
         <p
@@ -12,6 +33,18 @@ export default class Home extends Component {
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
         <Header />
+        <ul>
+          {categories.map((categorie) => (
+            <li key={ categorie.id }>
+              <button
+                type="button"
+                data-testid="category"
+              >
+                { categorie.name }
+              </button>
+            </li>
+          ))}
+        </ul>
       </section>
     );
   }
