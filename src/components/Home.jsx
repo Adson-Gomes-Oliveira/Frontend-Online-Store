d0c9d5f43cf1a3cbef6b246f235b7a353927618e
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2022-01-23 18:26:49 -0300

    alteraçoes requisito 05

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,40 @@
 // Esse código foi desenvolvido em conjunto( Luiz e Adson)
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './Header';
 import * as api from '../services/api';
 
-export default class Home extends Component {
+class Home extends React.Component {
   constructor() {
     super();
 
     this.state = {
       categories: [],
+      products: [],
+      id: '',
+      text: '',
     };
 
     this.getCategoriesList = this.getCategoriesList.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.getProductsList = this.getProductsList.bind(this);
   }
 
   componentDidMount() {
     this.getCategoriesList();
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      text: target.value,
+      id: target.id,
+    });
+  }
+
+  getProductsList = async () => {
+    const { text, id } = this.state;
+    const productList = await api.getProductsFromCategoryAndQuery(id, text);
+    console.log(productList.results);
+    this.setState({ products: productList.results });
   }
 
   getCategoriesList = async () => {
@@ -24,7 +43,7 @@ export default class Home extends Component {
   }
 
   render() {
-    const { categories } = this.state;
+    const { categories, text, products } = this.state;
     return (
       <section>
         <p
@@ -32,6 +51,20 @@ export default class Home extends Component {
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <input
+          data-testid="query-input"
+          onChange={ this.handleChange }
+          name="text"
+          type="text"
+          value={ text }
+        />
+        <button
+          data-testid="query-button"
+          onClick={ this.getProductsList }
+          type="button"
+        >
+          Pesquisar
+        </button>
         <Header />
         <ul>
           {categories.map((categorie) => (
@@ -45,7 +78,18 @@ export default class Home extends Component {
             </li>
           ))}
         </ul>
+        <ul>
+          {products.map(({ id, title, thumbnail, price }) => (
+            <li data-testid="product" key={ id }>
+              <h3>{title}</h3>
+              <img src={ thumbnail } alt={ title } />
+              <p>{price}</p>
+            </li>
+          ))}
+        </ul>
       </section>
     );
   }
 }
+
+export default Home;
